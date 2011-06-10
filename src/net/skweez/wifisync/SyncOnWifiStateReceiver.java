02c52318d0bdfd9aa00ab24246e47f0a787dc040
Michael Kanis <mks@skweez.net>
2011-06-10 23:24:38 +0200

    Initial commit.

---
+++ src/net/skweez/wifisync/SyncOnWifiStateReceiver.java
@@ -0,0 +1,43 @@
+package net.skweez.wifisync;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+public class SyncOnWifiStateReceiver extends BroadcastReceiver {
+
+	private static String TAG = "AutoSync";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		final String action = intent.getAction();
+
+		if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+
+			NetworkInfo networkInfo = (NetworkInfo) intent
+					.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+
+			if (networkInfo.getState().equals(State.CONNECTED)) {
+				ContentResolver.setMasterSyncAutomatically(true);
+				Log.d(TAG, "Auto sync enabled.");
+			}
+
+		} else if (action
+				.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)) {
+
+			boolean connected = intent.getBooleanExtra(
+					WifiManager.EXTRA_SUPPLICANT_CONNECTED, true);
+
+			if (connected == false) {
+				ContentResolver.setMasterSyncAutomatically(false);
+				Log.d(TAG, "Auto sync disabled.");
+			}
+		}
+	}
+}
