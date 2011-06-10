73fd4b173257d160237d61eda54df18771e0ac02
Michael Kanis <mks@skweez.net>
2011-06-10 23:40:13 +0200

    Minor code cleanup.

--- src/net/skweez/wifisync/SyncOnWifiStateReceiver.java
+++ src/net/skweez/wifisync/SyncOnWifiStateReceiver.java
@@ -11,7 +11,7 @@ import android.util.Log;
 
 public class SyncOnWifiStateReceiver extends BroadcastReceiver {
 
-	private static String TAG = "AutoSync";
+	private static String TAG = "WifiSync";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
