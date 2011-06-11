568baae3c898780bbd56c0d1639b929232c9a9a2
Michael Kanis <mks@skweez.net>
2011-06-11 16:06:56 +0200

    Add simple icon. Add some debugging output.

--- src/net/skweez/wifisync/SyncOnWifiStateReceiver.java
+++ src/net/skweez/wifisync/SyncOnWifiStateReceiver.java
@@ -7,20 +7,22 @@ import android.content.Intent;
 import android.net.NetworkInfo;
 import android.net.NetworkInfo.State;
 import android.net.wifi.WifiManager;
+import android.os.Bundle;
 import android.util.Log;
 
 public class SyncOnWifiStateReceiver extends BroadcastReceiver {
 
+	/** The log tag. */
 	private static String TAG = "WifiSync";
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(final Context context, final Intent intent) {
 
 		final String action = intent.getAction();
 
 		if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
 
-			NetworkInfo networkInfo = (NetworkInfo) intent
+			final NetworkInfo networkInfo = (NetworkInfo) intent
 					.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 
 			if (networkInfo.getState().equals(State.CONNECTED)) {
@@ -31,13 +33,29 @@ public class SyncOnWifiStateReceiver extends BroadcastReceiver {
 		} else if (action
 				.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)) {
 
-			boolean connected = intent.getBooleanExtra(
+			final boolean connected = intent.getBooleanExtra(
 					WifiManager.EXTRA_SUPPLICANT_CONNECTED, true);
 
 			if (connected == false) {
 				ContentResolver.setMasterSyncAutomatically(false);
 				Log.d(TAG, "Auto sync disabled.");
 			}
+
+		} else {
+
+			Log.d(TAG, "action: " + action);
+			debugExtras(intent);
+
+		}
+	}
+
+	/**
+	 * @param intent
+	 */
+	private void debugExtras(final Intent intent) {
+		Bundle extras = intent.getExtras();
+		for (String key : extras.keySet()) {
+			Log.d(TAG, key + "=" + extras.get(key));
 		}
 	}
 }
